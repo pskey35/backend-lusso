eb6265a3db85b83fbfbd7629bc063f5eaf6f6d6b
pskey35 <anonimo35371@gmail.com>
2024-10-23 12:58:40 -0500

    feat(app.js):agregue cors

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 const home = require("./routes/home")
 const auth = require("./routes/auth")
-
 app.use(express.json())
-
-
+const path = require('path');
+app.use(cors())
 //crear el endpoint de autenticacion
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+
 
 app.use(home,auth)
 
