9f77f6e4b02412651e554ea93c7482a3748da617
pskey35 <anonimo35371@gmail.com>
2024-10-23 13:00:38 -0500

    feat(home): se agrego el enpoint para consumir en el home

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,35 @@
 const express = require("express")
+const firebase = require("firebase/compat/app");
+require("firebase/compat/storage");
+const fs = require("fs")
+const path = require('path');
+
 
 const routerHome = express.Router()
+
+
 
 
 routerHome.get("/",(req,res)=>{
     res.json({
         saludo:"hola"
     })
+})
+
+
+routerHome.get("/allProducts",(req,res)=>{
+    const basePath = path.join(__dirname, '..', 'public'); // Ruta a la carpeta public
+    const folders = ['producto-1', 'producto-2', 'producto-3']; // Nombres de las carpetas
+
+    const products = folders.map(folder => {
+        const folderPath = path.join(basePath, folder);
+        const images = fs.readdirSync(folderPath).map(file => {
+            return `/public/${folder}/${file}`; // Generar la URL para cada imagen
+        });
+        return { folder, images };
+    });
+
+    res.json(products); // Devolver las imágenes como JSON
 })
 
 module.exports = routerHome
