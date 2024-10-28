cbedae8de573e32310e30823ad9ad639dc611715
pskey35 <anonimo35371@gmail.com>
2024-10-28 17:32:39 -0500

    feat(mysql.js): se agrego la configuracion para la base de datos

--- config/mysql.js
+++ config/mysql.js
@@ -2,11 +2,27 @@ import mysql from "mysql2"
 import dotenv from "dotenv"
 dotenv.config()
 
-export const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DATABASE_HOST,        // Cambia al host de tu base de datos
-  port: process.env.DATABASE_PORT,    
-  user: process.env.DATABASE_USER,   
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD, // Contraseña de la base de datos
-  database: process.env.DATABASE_SCHEMA     // Nombre de la base de datos a la que te conectarás
+  database: process.env.DATABASE_SCHEMA,    // Nombre de la base de datos a la que te conectarás
+  waitForConnections: true,
+  connectionLimit: 10000,  // Número máximo de conexiones en el pool
 });
 
+
+
+
+
+export const mysqlPromesa = (consulta,values)=>{
+  return new Promise((resolve,reject)=>{
+    connection.query(consulta,values,(err,query)=>{
+      if(err){
+        reject(err) 
+      }
+      resolve(query)
+    })
+  })
+}
