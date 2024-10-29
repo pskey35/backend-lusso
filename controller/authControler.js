92305a7baf9a68251067875de8f7fafec8491492
pskey35 <anonimo35371@gmail.com>
2024-10-29 13:03:18 -0500

    chore(authControler.js):ordenando codigo

--- controller/authControler.js
+++ controller/authControler.js
@@ -1,81 +1,88 @@
 import xss from "xss"
-import {User} from "../models/authModel.js"
+import { User } from "../models/authModel.js"
+import jwt from "jsonwebtoken"
+
 
 export const registerControler = async (req, res) => {
-   // const { user, password, telefono, address, edad} = req.body
-    //const userSano = xss(req.body.user.trim())
-
-
-
-    /*CALL registrar_usuario('Juan', 
-    'Pérez', 'juan@example.com', 'contraseñaSegura',
-     '123456789', 30);
-*/
-
-
-    const nombreSano = xss(req.body.name.trim())
+    const userSano = xss(req.body.name.trim())
+    const apellidoSano = xss(req.body.apellido.trim())
     const emailSano = xss(req.body.email.trim())
     const passwordSano = xss(req.body.password.trim())
+
+
+    //esto de aca son opcionales
     const telefonoSano = xss(req.body.telefono.trim())
     const edadSano = xss(req.body.edad.trim())
+    const direccionSano = xss(req.body.direccion.trim())
+    const ciudadSano = xss(req.body.cuidad.trim())
+    const departamentoSano = xss(req.body.departamento.trim())
+    const paisSano = xss(req.body.paid.trim())
+    const codigoPostalSano = xss(req.body.codigoPostal)
 
-    //si existe el user en la databse mandarle un error porque 
-    //no debe de haber mas de un usuario con el mismo nombre
 
-    
-    if (false) {
+
+    const boleanoEmail = User.correoExist(emailSano)
+    if (boleanoEmail) {
+        //si el email ya estaba registrado en la database entra aqui
         return res.status(401).json({
             error: true,
-            message: "Este usuario ya existe en la database prueba con otro nombre de usuario",
+            message: "Este email ya ha sido registrado prueba con otro"
         })
     }
 
 
-    let message;
-    let token;
-    let error;
-    //quiero validar el campo user para que no inyecten nada
-    if (userSano.length >= 100) {
-        //en el front tambien se tiene que validar lo de trim()
-        message = "Error su nombre de usuario no debe contener mas de 100 caracteres"
-        token = null
-        error = true
-    }
-    
-    /*else if (!(/.\w+\d\W.+/.test(passwordSano))) {
-        //"Si la cadena no coincide con el patrón definido por la expresión regular"
-        message = "La contrasena no es segura pruebe con otro"
-        token = null
-        error = true
-    }else if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test()){
-        //aqui tengo que ver si todo
-    }
-*/
-    //falta hacer validacion con REGEX en password
-    if(password){
-        //si la password 
-     
-    }
 
-    if (error) {
-        return res.status(400).json({
+    const { error, message } = User.isValid(userSano)
+
+    if (error == false) {
+        //si el nombre de usuario es valido entra aqui
+        res.status(200).json({
             error,
-            token,
-            message
+            message,
+        })
+    } else {
+        return res.status(401).json({
+            error,
+            message,
         })
     }
+
+
+
+    const passwordValid = User.isValidPassword(passwordSano)
+
+    if (passwordValid) {
+        //si password es valido entonces se registra en la database 
+        //y se le da un token jwt
+        const {error,message,id,usuario} = User.registrar_usuario([userSano, apellidoSano,
+            emailSano, passwordSano, telefonoSano, edadSano, direccionSano,
+            departamentoSano, paisSano, codigoPostalSano
+        ])
+
+
+        const token = jwt.sign({id,usuario},"secretKey")
+    
+        return res.json({error,message,token})
+    }
+
+
 
 
 
     //si no hay error entra aqui
     //hacer consulta de sql para recibir el id de tal usuario
+
     token = jwt.sign({ id: 12, usuario: "juanito" }, "secretKey")
     return res.status(200).json({
         error: false,
         token,
         message: "Se ha creado su cuenta exitosamente",
     })
+
+
+
 }
+
 
 const loginControler = (req, res) => {
     const { username, password } = req.body;
@@ -101,4 +108,4 @@ const loginControler = (req, res) => {
 }
 
 
-export default {registerControler,loginControler}
+export default { registerControler, loginControler }
