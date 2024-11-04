c5bf4cbdea417e9da0c4174bb1ae4afb9dd50a03
pskey35 <anonimo35371@gmail.com>
2024-11-04 17:05:03 -0500

    chore(dashboard.js):como es donde el lugar en donde se mostrara el perfil de tal usuario se usara el authToken como middleware por seguridad

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,16 +1,16 @@
+//este dashboard se usa en la ruta /dashboard cuando el usuario quiere ver su perfil
 import express from "express"
 import {dashboardController} from "../controller/dashboardControler.js"
 import multer from "multer"
-
+import {authToken} from "../utils/middlewareAuth.js"
 
 const router = express.Router()
-
 const memoria = multer.memoryStorage()
 const upload = multer({storage:memoria})
 
 
 
-router.post("/dashboard",upload.single("image"),dashboardController)
+router.post("/dashboard",authToken,upload.single("image"),dashboardController)
 
 
 
