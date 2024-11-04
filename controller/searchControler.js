2f5f266cdd43ace22d7a423c1a91208737c6e7e4
pskey35 <anonimo35371@gmail.com>
2024-11-04 17:41:54 -0500

    chore(searchControler.js)

--- controller/searchControler.js
+++ controller/searchControler.js
@@ -1,7 +1,19 @@
+import {mysqlPromesa} from "../config/mysql.js"
 
 
-/*
-export const searchControler = (req,res)=>{
-    //necesito validar req las queries /search params query
-    console.log(req.query)
-}*/
+//aqui se maneja toda la busqueda
+export const searchControler = async (req, res) => {
+    const textSearch = req.query.textSearch
+    const size = req.query.size || 7
+    const page = req.query.page || 1 //esto pasa a la derecha si es falsy
+
+
+    //con la busqueda que hace el usuario se tiene que traer todos los datos que 
+    //coincidan de la database con el texto "polera", usar offsetop y like en database
+    //aqui morty me dara un procedure stored
+
+
+    //retornar nombreProducto,id,imagen url,categoria producto
+    
+}
+
