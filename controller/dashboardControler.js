89d69cbb938e077f7fbb60f673420b03b2e36fbd
pskey35 <anonimo35371@gmail.com>
2024-11-04 17:35:53 -0500

    feat(dashboardControler.js): ahora se sube los datos del dashboard, la imagen que se sube se guarda en cloudinary

--- controller/dashboardControler.js
+++ controller/dashboardControler.js
@@ -1,6 +1,49 @@
+import { v2 as cloudinary } from "cloudinary"
+import xss from "xss"
 
-export const dashboardController = (req,res)=>{
-    console.log(req.file)
-    console.log(req.body.message)
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+/*
+ el dashboard debe tener 
+ //categoria, producto, imagen productos, banners, agregar admins
+
+ se debe de crear un endpoint donde se reciba el file,categoria, nombreProducto,
+ imagen productos(solo url despues de haber subido a cloudinary)
+ primero subimos  a cloudinary luego esa url obtenida la subimos a database
+ */
+
+
+export const dashboardController = async (req, res) => {
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+                { folder: "josue" }, // Opcional: puedes especificar una carpeta
+                (error, result) => {
+                    if (error) reject(error);
+                    else resolve(result);
+                }
+            );
+            console.log(req.file)
+            stream.end(req.file.buffer); // Aquí enviamos el buffer directamente
+        });
+
+
+        const categoria = xss(req.body.categoria)
+        const nombreProducto = xss(req.body.nombreProducto)
+        const urlImageUploaded = result.url 
+        
+        res.status(200).json({ message: "valid",categoria,nombreProducto,urlImageUploaded})
+
+
+    } catch (error) {
+        console.log("hubo un error...")
+        console.log(error)
+        res.status(400).json({ message: "error" })
+    }
 }
 
