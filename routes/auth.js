c6bf30d3c3592f622d462c83c474c4a7061d3db3
pskey35 <anonimo35371@gmail.com>
2024-11-04 17:22:24 -0500

    docs(auth.js)

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+//en el authToken se maneja lo que vendria a ser todo sobre autenticacion del usuario
 import express from "express"
 import controler from "../controller/authControler.js"
 import {authToken} from "../utils/middlewareAuth.js"
@@ -11,4 +12,16 @@ routerAuth.post("/login",controler.loginControler)
 routerAuth.post("/register",controler.registerControler)
 
 
+
+
+routerAuth.post("/forgot-password",authToken,controler.forgotPassword)
+//reset password
+//change password
+
+
+
+/*si se quiere reseat el password entonces seria primero el usuario
+da click en resetear su password luego en el backend se llega esa peticion y se le envia algo 
+al correo un codigo como tal para reiniciar su password
+*/
 export default routerAuth
