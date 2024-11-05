8668b72c12f569c10cb47092949a6bb782007fbd
pskey35 <anonimo35371@gmail.com>
2024-11-05 08:58:50 -0500

    feat(search.js): se ha agregado la ruta search

--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,8 @@
-//import express from "express"
-//import controler from "../controller/"
-//se necesita que de la base de datos traiga todas las imagenes
-//que coincidan con el texto "telefono" usa LIKE de sql xd
+import express from "express"
+import {searchControler} from "./controller/searchControler.js"
 
-//se necesita que de la base de datos te traiga todas las url de las imagenes,
-    //nombre,producto,precio y descripcion
-    //y solo si tal imagen tiene como categoria "nuevos" 
 
-//const router = express.Router()
-//aqui se necesita obtener params querys req.query /saerch?text='telefono' algo asi
-//router.get("/search",controler.search)
+export const router = express.Router()
 
-//export default router;
+router.get("/search",searchControler)
 
