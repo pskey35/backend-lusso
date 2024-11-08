f54cf45bb47c5e84c2e10ccc6deaa861b670f2c8
pskey35 <anonimo35371@gmail.com>
2024-11-08 15:35:35 -0500

    feat(controller/productos.js):Added method postInsertProduct

--- controller/productos.js
+++ controller/productos.js
@@ -36,6 +36,40 @@ export default class Products {
 
         return res.json({error, message,data})
     }
+
+
+    static async postInsertProduct(req,res){
+
+        const dataPostBody = {
+            name:req.body.name,
+            description:req.body.description,
+            price:req.body.price,
+            stock:req.body.stock,
+            principal_img:req.body.principal_img,
+            brand:req.body.brand,
+            model:req.body.model,
+            dimention:req.body.dimencion,
+            weight:req.body.weight,
+            color:req.body.color,
+            material:req.body.material,
+            state:req.body.state,
+            category_id:req.body.category_id
+        }
+
+
+        const {error,data,message} = await ModelProducts.postInsertProduct(dataPostBody)
+        
+        if(error){
+            return res.status(404).json({error,message})
+        }
+
+
+      
+        res.status(200).json({error,message,data})
+
+
+    
+    }
 }
 
 
