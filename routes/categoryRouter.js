67cea145e927cc18a3101cb4f34f64cfb255ce9f
pskey35 <anonimo35371@gmail.com>
2024-11-12 19:00:27 -0500

    chore(categoryRouter.js)

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import ControllerCategory from "../controller/categoriasController.js"
+import ControllerCategory from "../controller/categoriesController.js"
 
 
 const router = express.Router()
@@ -8,8 +8,9 @@ const router = express.Router()
 router.get("/categorias", (req,res)=>ControllerCategory.readAllCategories(req,res))
 router.get("/categoria/:id_categoria", (req,res)=>ControllerCategory.selectByIdCategories(req,res))
 router.post("/deleteCategoria", (req,res)=>ControllerCategory.deleteByIdCategories(req,res))
+
 router.post("/addCategoria",(req,res)=> ControllerCategory.addCategories(req,res))
-router.post("/editCategoria",(req,res)=> ControllerCategory.editByIdCategories(req,res))
+router.post("/editCategory",(req,res)=> ControllerCategory.editCategoryById(req,res))
 
 
 
