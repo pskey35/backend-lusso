965ac6fd7c407a4574e48bdbd0a2767c9dd268a9
pskey35 <anonimo35371@gmail.com>
2024-11-14 14:54:20 -0500

    feat(productsModel.js): i ended functions like deleteAllImagesProductById, addImageProduct

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,4 +1,9 @@
 import { mysqlPromesa } from "../config/mysql.js"
+import { v2 as cloudinary } from "cloudinary"
+import dotenv from "dotenv"
+
+
+dotenv.config()
 
 export default class ModelProducts {
     static async getAllProducts() {
@@ -129,16 +134,16 @@ export default class ModelProducts {
         )
 
 
-        
+
         const [resu] = await mysqlPromesa("SELECT @problem AS error,@message AS message")
 
 
-        if(resu.error == 0){
-            return {error:false,message:resu.message}
+        if (resu.error == 0) {
+            return { error: false, message: resu.message }
         }
 
 
-        return {error:true,message:resu.message}
+        return { error: true, message: resu.message }
 
 
 
@@ -149,5 +154,111 @@ export default class ModelProducts {
 
 
     }
+
+
+
+    static async getAllImagesProductById(dataReceived) {
+
+        try {
+            const idParams = dataReceived
+
+
+            const data = await mysqlPromesa("call leer_imagenes_producto(?, @problem, @message)", [idParams])
+
+            const [result] = await mysqlPromesa("SELECT @problem AS error,@message AS message")
+
+
+            if (result.error == 0) {
+                //aqui entra si no hay error
+                console.log("no hay error")
+                return { error: false, message: result.message, data }
+            }
+
+
+
+            return { error: result.error, message: result.message }
+        } catch (error) {
+            console.log("hubo un error")
+            console.log(error)
+            return { error: true, message: error }
+        }
+
+
+
+
+    }
+
+
+    static async addImageProduct(dataReceived) {
+        try {
+            const fileBuffer = dataReceived.fileBuffer
+
+            const result = await new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    { folder: "josue" }, // Opcional: puedes especificar una carpeta
+                    (error, result) => {
+                        if (error) reject(error);
+                        else resolve(result);
+                    }
+                );
+
+                stream.end(fileBuffer); // Aquí enviamos el buffer directamente
+            });
+
+
+
+            //idProduct to insert image
+            const idProduct = dataReceived.idParams
+
+            const urlImage = result.url
+            const nameFile = result.display_name
+
+            await mysqlPromesa(`call insertar_imagen(?, ?, ?,@problem, @message)`, [idProduct, nameFile, urlImage])
+
+
+            const [data] = await mysqlPromesa("SELECT @problem AS error,@message AS message")
+
+
+            if (data.error == 1) {
+                return { error: true, message: data.message }
+            }
+
+
+
+
+            return { error: data.error, message: data.message }
+        } catch (error) {
+
+            return { error: true, message: error }
+
+        }
+    }
+
+
+    static async deleteAllImagesProductById(dataReceived) {
+        try {
+
+            const idParams = dataReceived
+
+            await mysqlPromesa("call eliminar_imagen_producto(?,@problem,@message)", [idParams])
+            const [result] = await mysqlPromesa("SELECT @problem AS error,@message AS message")
+
+
+            console.log("models")
+            console.log(result)
+
+            if (result.error == 0) {
+                return { error: false, message: result.message }
+            }
+
+
+            return { error: true, message: result.message}
+
+
+        } catch (error) {
+            return { error: true, message: error }
+        }
+    }
+
 
 }
