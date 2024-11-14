951f0eb0857370b37df08d420e93b0fb80f0b863
pskey35 <anonimo35371@gmail.com>
2024-11-14 13:10:16 -0500

    chore(cartShop): new files

---
+++ routes/cartShopRouter.js
@@ -0,0 +1,19 @@
+import express from "express"
+//un router para agregar productos eliminar restar aumentar saber precio del carrito
+
+
+
+const router = express.Router()
+
+/*
+    crear token carrito create
+*/
+
+router.get("/create")
+
+
+
+
+export default router
+
+
